be408fc7ad7dc6325e71c9e3907e66dcafae3cd6
RohitNandhan <98014469+RohitNandhan@users.noreply.github.com>
2022-02-02 22:29:51 +0530

    Update PatternDesign.java

--- PatternDesign.java
+++ PatternDesign.java
@@ -4,49 +4,49 @@ public class PatternDesign {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-for(int i=1;i<2;i++)
+for(int i=1;i<2;i++)			//optional loop
 {
-	for(int j=1;j<=1;j++)
+	for(int j=1;j<=1;j++)			//loops 1 times
 	{
 	System.out.print("*");
 	}
 	System.out.println();
-	for(int j=1;j<=2;j++)
+	for(int j=1;j<=2;j++)			//loops 2 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=3;j++)
+	for(int j=1;j<=3;j++)			//loops 3 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=4;j++)
+	for(int j=1;j<=4;j++)			//loops 4 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=5;j++)
+	for(int j=1;j<=5;j++)			//loops 5 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=4;j++)
+	for(int j=1;j<=4;j++)			//loops 4 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=3;j++)
+	for(int j=1;j<=3;j++)			//loops 3 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=2;j++)
+	for(int j=1;j<=2;j++)			//loops 2 times
 	{
 		System.out.print("*");
 }
 	System.out.println();
-	for(int j=1;j<=1;j++)
+	for(int j=1;j<=1;j++)			//loops 1 times
 	{
 	System.out.print("*");
 	}
