e14572a69ed9fe4596b7cc55a78b7c0250d34b42
RohitNandhan <98014469+RohitNandhan@users.noreply.github.com>
2022-02-02 22:21:08 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -7,32 +7,32 @@ public class Pattern2 {
 		
 		for(int i=1;i<2;i++)
 		{
-			System.out.print("          ");
-			for(int j=1;j<=1;j++)
+			System.out.print("          ");//10 spaces
+			for(int j=1;j<=1;j++)			//loops 1 times
 			{
 			System.out.print("*");
 			}
 			System.out.println();
-			System.out.print("         ");
-			for(int j=1;j<=2;j++)
+			System.out.print("         ");//9spaces
+			for(int j=1;j<=2;j++)			//loops 2 times
 			{
 			System.out.print("*");
 			}
 			System.out.println();
-			System.out.print("        ");
-			for(int j=1;j<=3;j++)
+			System.out.print("        ");//8 spaces
+			for(int j=1;j<=3;j++)			//loops 3 times
 			{
 			System.out.print("*");
 			}
 			System.out.println();
-			System.out.print("       ");
-			for(int j=1;j<=4;j++)
+			System.out.print("       ");//7 spaces
+			for(int j=1;j<=4;j++)			//loops 4 times
 			{
 			System.out.print("*");
 			}
 			System.out.println();
-			System.out.print("      ");
-			for(int j=1;j<=5;j++)
+			System.out.print("      ");//6 spaces
+			for(int j=1;j<=5;j++)			//loops 5 times
 			{
 			System.out.print("*");
 			}
