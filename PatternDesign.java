5831f955c5a9e837b8970209b983a9e60c9973c8
RohitNandhan <98014469+RohitNandhan@users.noreply.github.com>
2022-02-02 22:02:13 +0530

    Add files via upload

---
+++ PatternDesign.java
@@ -0,0 +1,39 @@
+package PracticePrograms;
+
+public class PatternDesign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<2;i++)
+{
+	for(int j=1;j<=1;j++)
+	{
+	System.out.print("*");
+	}
+	System.out.println();
+	for(int j=1;j<=2;j++)
+	{
+		System.out.print("*");
+}
+	System.out.println();
+	for(int j=1;j<=3;j++)
+	{
+		System.out.print("*");
+}
+	System.out.println();
+	for(int j=1;j<=4;j++)
+	{
+		System.out.print("*");
+}
+	System.out.println();
+	for(int j=1;j<=5;j++)
+	{
+		System.out.print("*");
+}
+	System.out.println();
+
+	
+	}
+
+}
+}
